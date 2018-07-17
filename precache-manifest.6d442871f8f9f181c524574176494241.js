60cdad23aba4988c2c70da344c97604cad574eed
Mateusz Bałdyga <matbaldyga@gmail.com>
2018-07-17 19:19:14 +0200

    ccb03bd

---
+++ precache-manifest.6d442871f8f9f181c524574176494241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "845f376ed211786a08e8a0993cf763f0",
+    "url": "/sprites.svg"
+  },
+  {
+    "revision": "e6fd35933b14a96225ae",
+    "url": "/js/chunk-vendors.e6fd3593.js"
+  },
+  {
+    "revision": "b565f77818910cf3f797",
+    "url": "/js/app.b565f778.js"
+  },
+  {
+    "revision": "525d9115d8be4b289223d5609666746d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b565f77818910cf3f797",
+    "url": "/css/app.4533df19.css"
+  }
+];
